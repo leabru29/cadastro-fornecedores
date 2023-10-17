e07eeaad1e442727b9d233dfca23fbcd1b4fdb81
Leandro <leandro_ccb10@hotmail.com>
2023-10-17 12:00:40 -0300

    Modelo de Fornecedor

--- app/Models/Fornecedor.php
+++ app/Models/Fornecedor.php
@@ -2,10 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Fornecedor extends Model
 {
     use HasFactory;
+
+    use HasUuids;
+
+    protected $table = 'fornecedors';
+
+    public $incrementing = false;
+
 }
