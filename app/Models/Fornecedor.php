beff0d52eed390d0a8c0cb030f82a49401c6b14a
Leandro <leandro_ccb10@hotmail.com>
2023-10-18 11:14:06 -0300

    Correcao softDelete

--- app/Models/Fornecedor.php
+++ app/Models/Fornecedor.php
@@ -5,19 +5,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Fornecedor extends Model
 {
     use HasFactory;
 
     use HasUuids;
+
+    use SoftDeletes;
     
 
     protected $table = 'fornecedors';
 
     public $incrementing = false;
-
-    protected $softDelete = true;
 
     protected $fillable = [
         "nome",
