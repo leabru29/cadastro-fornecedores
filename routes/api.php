44d24ac080adaf4cccfb47f54b3862a1db64d318
Leandro <leandro_ccb10@hotmail.com>
2023-10-20 10:08:28 -0300

    Protegendo Rota

--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('fornecedores', ApiFornecedorController::class);
+Route::group(['middleware' => 'auth:sanctum'], function() {
+    Route::apiResource('fornecedores', ApiFornecedorController::class);
+});
